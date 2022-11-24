cc919ca26190dbc681ecfa8ec32721551f2c9991
chobin <chobin.to.chobin@gmail.com>
2022-11-24 10:06:28 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <section>
+        <h1>見出し</h1>
+        <p>段落</p>
+        <p class="notes">注意書き</p>
+        <ul>
+          <li>リスト</li>
+        </ul>
+      </section>
+      <section>
+        <h1>見出し</h1>
+        <p>段落</p>
+      </section>
     </div>
   );
 }
